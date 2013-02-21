b1eb4f5f9a1816c92a16557051678324ad896999
xiaojin.pku@gmail.com <xiaojin.pku@gmail.com@ab26c6af-f694-533d-1c2a-a5837638ddbf>
2013-02-21 06:16:28 +0000

    上传文件接口 完毕！

---
+++ branches/V1/src/com/psr/util/FileUtil.java
@@ -0,0 +1,87 @@
+package com.psr.util;
+
+import java.io.*;
+import java.io.IOException;
+
+public class FileUtil {
+	private static final String ProjectName = "PSRWS";
+	private static final String RootFolder ="UpFolder";
+
+	/**
+	 * 获得指定文件的byte数组
+	 */
+	static public byte[] getBytes(String filePath) {
+		byte[] buffer = null;
+		try {
+			File file = new File(filePath);
+			FileInputStream fis = new FileInputStream(file);
+			ByteArrayOutputStream bos = new ByteArrayOutputStream(1000);
+			byte[] b = new byte[1000];
+			int n;
+			while ((n = fis.read(b)) != -1) {
+				bos.write(b, 0, n);
+			}
+			fis.close();
+			bos.close();
+			buffer = bos.toByteArray();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return buffer;
+	}
+
+	/**
+	 * 根据byte数组，生成文件
+	 */
+	public static void GenFile(byte[] bfile, String filePath, String fileName) {
+		BufferedOutputStream bos = null;
+		FileOutputStream fos = null;
+		File file = null;
+		try {
+			filePath = getPorjectPath() + "\\" + RootFolder + "\\" + filePath;
+
+			File dir = new File(filePath);
+			dir.mkdirs();
+			if (!dir.exists() && dir.isDirectory()) {// 判断文件目录是否存在
+				System.out.println("====================================================================");
+				System.out.println(filePath);
+				System.out.println("====================================================================");
+				dir.mkdirs();
+			}
+			file = new File(filePath + "\\" + fileName);
+			fos = new FileOutputStream(file);
+			bos = new BufferedOutputStream(fos);
+			bos.write(bfile);
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (bos != null) {
+				try {
+					bos.close();
+				} catch (IOException e1) {
+					e1.printStackTrace();
+				}
+			}
+			if (fos != null) {
+				try {
+					fos.close();
+				} catch (IOException e1) {
+					e1.printStackTrace();
+				}
+			}
+		}
+	}
+
+	// 获取当前项目的绝对路径
+	static String getPorjectPath() {
+		String nowpath; // 当前tomcat的bin目录的路径 如
+						// D:\java\software\apache-tomcat-6.0.14\bin
+		String tempdir;
+		nowpath = System.getProperty("user.dir");
+		tempdir = nowpath.replace("bin", "webapps"); // 把bin 文件夹变到 webapps文件里面
+		tempdir += "\\" + ProjectName; // 拼成D:\java\software\apache-tomcat-6.0.14\webapps\ProjectName
+		return tempdir;
+	}
+}
