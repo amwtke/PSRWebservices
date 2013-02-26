392d54897fbef365c7fbfa719abb091f30c1f313
xiaojin.pku@gmail.com <xiaojin.pku@gmail.com@ab26c6af-f694-533d-1c2a-a5837638ddbf>
2013-02-26 14:58:44 +0000

    修改上传文件更新地址 自动加入前缀

--- src/com/psr/util/FileUtil.java
+++ src/com/psr/util/FileUtil.java
@@ -6,7 +6,7 @@ import java.io.IOException;
 public class FileUtil {
 	private static final String ProjectName = "psr";
 	private static final String RootFolder ="files";
-
+	public static final String PSRFileFolderWithOutEndSlash = getPorjectPath() + "\\" + RootFolder;
 	/**
 	 * 获得指定文件的byte数组
 	 */
@@ -75,7 +75,7 @@ public class FileUtil {
 	}
 
 	// 获取当前项目的绝对路径
-	static String getPorjectPath() {
+	public static String getPorjectPath() {
 		String nowpath; // 当前tomcat的bin目录的路径 如
 						// D:\java\software\apache-tomcat-6.0.14\bin
 		String tempdir;
