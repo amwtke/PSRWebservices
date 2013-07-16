a45ddae88d21b738bcc001106aa59a9c6fcee095
xiaojin.pku@gmail.com <xiaojin.pku@gmail.com@ab26c6af-f694-533d-1c2a-a5837638ddbf>
2013-07-16 08:35:21 +0000

    GetOperationClass.class都直接用recordid来取数据

--- branches/V1/src/com/psr/record/GetOperationClass.java
+++ branches/V1/src/com/psr/record/GetOperationClass.java
@@ -11,13 +11,19 @@ import com.psr.util.JDBCUtil;
 @WebService(targetNamespace = "http://record.psr.com/", endpointInterface = "com.psr.record.GetOperationSEI", portName = "GetOperationClassPort", serviceName = "GetOperationClassService")
 public class GetOperationClass implements GetOperationSEI {
 
-//	public static void main(String args[]) throws ClassNotFoundException, SQLException
-//	{
-//		GetOperationClass op = new GetOperationClass();
-//		ArrayList<Record> _records = op.GetRecords("sd");
-//		if(_records==null || _records.size()==0)
-//			System.out.print("无数据");
-//	}
+//	 public static void main(String args[]) throws ClassNotFoundException,
+//	 SQLException
+//	 {
+//	 GetOperationClass op = new GetOperationClass();
+//	 ArrayList<Fact> _records = op.GetFacts("QN10330工程公司licl006");
+//	 ArrayList<RecordResult> _recordresult = op.GetResults("QN10330工程公司licl006");
+//	 if(_records==null || _records.size()==0)
+//		 System.out.print("无数据");
+//	 System.out.println(_records.get(0).getContent());
+//	 System.out.println(_recordresult.get(0).getContent());
+//	 }
+	 
+	 
 	public ArrayList<Fact> GetFacts(String recordNo)
 			throws ClassNotFoundException, SQLException {
 		if (recordNo != null) {
@@ -25,18 +31,19 @@ public class GetOperationClass implements GetOperationSEI {
 			java.sql.ResultSet rs = null;
 			try {
 				st = JDBCUtil.GetConnection().createStatement();
-				String sql = "select * from PSR_FACT where RECORDNO=\'"+recordNo+"\'";
+				String recordId = GetRecords(recordNo).get(0).getId();
+				String sql = "select * from PSR_FACT where RECORDID=\'" + recordId
+						+ "\'";
 				rs = st.executeQuery(sql);
-				if(rs!=null)
-				{
-					ArrayList<Fact> _retList =new ArrayList<>();
-					while(rs.next())
-					{
+				
+				if (rs != null) {
+					ArrayList<Fact> _retList = new ArrayList<>();
+					while (rs.next()) {
 						Fact temp = new Fact();
 						temp.setId(rs.getString("ID"));
-						if(rs.getDate("CREATE_DATE")!=null)
+						if (rs.getDate("CREATE_DATE") != null)
 							temp.setCreateDate(rs.getDate("CREATE_DATE"));
-						
+
 						temp.setCreateUser(rs.getString("CREATE_USER"));
 						temp.setAssociateResult(rs.getInt("ASSOCIATERESULT"));
 						temp.setContent(rs.getString("CONTENT"));
@@ -60,7 +67,7 @@ public class GetOperationClass implements GetOperationSEI {
 		}
 		return null;
 	}
-	
+
 	public ArrayList<RecordResult> GetResults(String recordNo)
 			throws ClassNotFoundException, SQLException {
 		if (recordNo != null) {
@@ -68,26 +75,27 @@ public class GetOperationClass implements GetOperationSEI {
 			java.sql.ResultSet rs = null;
 			try {
 				st = JDBCUtil.GetConnection().createStatement();
-				String sql = "select * from PSR_RECORDRESULT where RECORDNO=\'"+recordNo+"\'";
+				String recordId = GetRecords(recordNo).get(0).getId();
+				String sql = "select * from PSR_RECORDRESULT where RECORDID=\'" + recordId
+						+ "\'";
 				rs = st.executeQuery(sql);
-				if(rs!=null)
-				{
-					ArrayList<RecordResult> _retList =new ArrayList<>();
-					while(rs.next())
-					{
+				
+				if (rs != null) {
+					ArrayList<RecordResult> _retList = new ArrayList<>();
+					while (rs.next()) {
 						RecordResult temp = new RecordResult();
 						temp.setId(rs.getString("ID"));
-						if(rs.getDate("CREATE_DATE")!=null)
+						if (rs.getDate("CREATE_DATE") != null)
 							temp.setCreateDate(rs.getDate("CREATE_DATE"));
-						
+
 						temp.setCreateUser(rs.getString("CREATE_USER"));
-						
+
 						temp.setIsFH(rs.getBoolean("ISFH"));
 						temp.setIsPC(rs.getBoolean("ISPC"));
-						
+
 						temp.setContent(rs.getString("CONTENT"));
 						temp.setRecordID(rs.getString("RECORDID"));
-						
+
 						temp.setFactNos(rs.getString("FACTNOS"));
 						temp.setResultNo(rs.getInt("RESULTNO"));
 						temp.setRecordNo(rs.getString("RECORDNO"));
@@ -107,7 +115,7 @@ public class GetOperationClass implements GetOperationSEI {
 		}
 		return null;
 	}
-	
+
 	public ArrayList<Record> GetRecords(String recordNo)
 			throws ClassNotFoundException, SQLException {
 		if (recordNo != null) {
@@ -115,18 +123,17 @@ public class GetOperationClass implements GetOperationSEI {
 			java.sql.ResultSet rs = null;
 			try {
 				st = JDBCUtil.GetConnection().createStatement();
-				String sql = "select * from PSR_RECORD where RECORDNO=\'"+recordNo+"\'";
+				String sql = "select * from PSR_RECORD where RECORDNO=\'"
+						+ recordNo + "\'";
 				rs = st.executeQuery(sql);
-				if(rs!=null)
-				{
-					ArrayList<Record> _retList =new ArrayList<>();
-					while(rs.next())
-					{
+				if (rs != null) {
+					ArrayList<Record> _retList = new ArrayList<>();
+					while (rs.next()) {
 						Record temp = new Record();
 						temp.setId(rs.getString("ID"));
-						if(rs.getDate("CREATE_DATE")!=null)
+						if (rs.getDate("CREATE_DATE") != null)
 							temp.setCreateDate(rs.getDate("CREATE_DATE"));
-						
+
 						temp.setCreateUser(rs.getString("CREATE_USER"));
 						temp.setRecordNo(rs.getString("RECORDNO"));
 						temp.setApproveBy(rs.getString("APPROVERBY"));
@@ -138,21 +145,25 @@ public class GetOperationClass implements GetOperationSEI {
 						temp.setInputTime(rs.getDate("INPUTTIME"));
 						temp.setInputUSer(rs.getString("INPUTUSER"));
 						temp.setInputUSerSuoXie(rs.getString("INPUTUSERSUOXIE"));
-						
-						temp.setIsActivityInspection(rs.getBoolean("ISACTIVITYINSPECTION"));
-						temp.setIsCommunication(rs.getBoolean("ISCOMMUNICATION"));
+
+						temp.setIsActivityInspection(rs
+								.getBoolean("ISACTIVITYINSPECTION"));
+						temp.setIsCommunication(rs
+								.getBoolean("ISCOMMUNICATION"));
 						temp.setIsCompliance(rs.getBoolean("ISCOMPLIANCE"));
 						temp.setIsWalkDown(rs.getBoolean("ISWALKDOWN"));
-						temp.setIsDocumentRevision(rs.getBoolean("ISDOCUMENTREVISION"));
-						
-						temp.setRecordDeSciption(rs.getString("RECORDDESCIPTION"));
+						temp.setIsDocumentRevision(rs
+								.getBoolean("ISDOCUMENTREVISION"));
+
+						temp.setRecordDeSciption(rs
+								.getString("RECORDDESCIPTION"));
 						temp.setSendBackReaSon(rs.getString("SENDBACKREASON"));
 						temp.setStatus(rs.getString("PSR_STATUS"));
 						temp.setTopic(rs.getString("TOPIC"));
 						temp.setTopicDesc(rs.getString("TOPICDESC"));
 						temp.setYaoSubianhao(rs.getString("YAOSUBINHAO"));
 						temp.setZhuangtibianhao(rs.getString("ZHUANGTIBIANHAO"));
-						
+
 						_retList.add(temp);
 					}
 					return _retList;
